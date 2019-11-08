c4c40225167a5a2f9b068f2b9b8080a2f0f9d17e
Zeljko Livaja <44612372+zeljkolivaja@users.noreply.github.com>
2019-11-08 20:25:15 +0100

    visual update leagues/index

--- resources/views/leagues/index.blade.php
+++ resources/views/leagues/index.blade.php
@@ -3,20 +3,38 @@
 
 @section('content')
 
-@foreach ($leagues as $league)
+{{-- @foreach ($leagues as $league)
 <li>
 
 
-<a href="/leagues/{{$league->id}}">
-         {{$league->leagueName}} </a>
-        <br/>
-        </li>
+    <a href="/leagues/{{$league->id}}">
+        {{$league->leagueName}} </a>
+    <br />
+</li>
 
 
-@endforeach
+@endforeach --}}
+
+
+<div class="row">
+
+@foreach ($leagues as $league)
+
+        <div class="col-sm-4">
+          <div class="card">
+            <div class="card-body">
+              <h5 class="card-title">{{$league->leagueName}}</h5>
+              <p class="card-text">{{$league->description}}</p>
+              <a href="/leagues/{{$league->id}}" class="btn btn-primary">Menage League</a>
+            </div>
+          </div>
+        </div>
 
 
 
+
+ @endforeach
+</div>
 
 
 

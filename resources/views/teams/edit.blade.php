d1fa8f19aa43e7bb14c7f8da36180e122e04ba5e
Zeljko Livaja <44612372+zeljkolivaja@users.noreply.github.com>
2019-11-13 16:46:51 +0100

    added some bootstrap to teams/edit

--- resources/views/teams/edit.blade.php
+++ resources/views/teams/edit.blade.php
@@ -1,11 +1,22 @@
+@extends('layout')
+
+@section('content')
+<br />
 
 @if (session('message'))
 <p> {{ session('message')}} </p>
 @endif
 
-<form action="/teams/{{$team->id}}" method="post">
-{{ method_field("PATCH") }}
-@csrf
-<input type="text" name="teamName" value='{{$team->teamName}}' required>
-<input type="submit" value="Edit Team">
+
+<form class="form-inline" action="/teams/{{$team->id}}" method="post">
+    {{ method_field("PATCH") }}
+    @csrf
+    <label class="sr-only" for="inlineFormInputName2">Name</label>
+    <input type="text" class="form-control mb-2 mr-sm-2" id="inlineFormInputName2" name="teamName"
+        value='{{$team->teamName}}' required">
+
+    <button type="submit" class="btn btn-primary mb-2">Submit</button>
 </form>
+
+
+@endsection
